29cb3e00090820a2a15a83566f34e0e9f265b825
Kory Stone <kory.stone@BLUHORSE.LOCAL>
2021-06-04 11:11:25 -0500

    Added img cards on the home page

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,15 +6,17 @@ function CardItem(props) {
 		<>
 			<li className='cards_item'>
 				<Link className='cards__item__link' to={props.path}>
-					<figure className='cards__item__pic-wrap'>
+					<figure
+						className='cards__item__pic-wrap'
+						data-category={props.label}>
 						<img
-							src='/'
+							src={props.src}
 							alt='Travel Image'
 							className='cards__item__img'
 						/>
 					</figure>
 					<div className='cards__item__info'>
-						<h5 className='cards__item__text'></h5>
+						<h5 className='cards__item__text'>{props.text}</h5>
 					</div>
 				</Link>
 			</li>
